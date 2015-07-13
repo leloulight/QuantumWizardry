d0dc8fd213ca878f2ad1b0089eb4ba0973f3741c
MESSpace <MESSpace@users.noreply.github.com>
2015-07-12 21:27:56 -0700

    Draft code for sensing awakened world
    Will have to make an event handler for these functions to run on world load, but that can come later.

--- src/main/java/messpace/QuantumWizardry/awakened/AwakenedBeachOverride.java
+++ src/main/java/messpace/QuantumWizardry/awakened/AwakenedBeachOverride.java
@@ -11,6 +11,21 @@ public class AwakenedBeachOverride extends BiomeGenBeach{
 		// TODO Auto-generated constructor stub
 	}
 	
+	//Below is the proposed change to code in order to detect whether the world is in the awakened state.
+	//Will implement if working, when Quantum Wizard is implemented, and when all override classes are written.
+	/*public static setPurpleSkies(NBTTagCompound nbt) {
+		boolean isAwakened = nbt.getString("isAwakened");
+		if(isAwakened == true) {
+			System.out.println("World is in awakened state, skies will be purple.");
+			@SideOnly(Side.CLIENT)
+			public int getSkyColorByTemp(float par1) {
+				return 0XE64CE6;
+			}
+		}else{
+			System.out.println("World is not in awakened state, skies will be as normal.");
+		}
+	}*/
+	
 	@SideOnly(Side.CLIENT)
 	public int getSkyColorByTemp(float par1) {
 		return 0xE64CE6;
